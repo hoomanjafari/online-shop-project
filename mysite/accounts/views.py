4b98693c95bda2fc60e2c64c09cd00e176f65b98
hooman <hooman.jafari3647@yahoo.com>
2023-12-07 23:38:31 +0330

    signup, login, backend

---
+++ mysite/accounts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from .forms import UserRegisterForm, UserLoginForm
+
+
+class UserRegisterView(View):
+    def post(self, request):
+        form = UserRegisterForm(self.request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(username=cd['email'], password=cd['password'])
+            return redirect('home:home')
+        else:
+            return render(request, 'home/index.html', UserRegisterForm())
+
+
+class UserLoginView(View):
+    def post(self, request):
+        form = UserLoginForm(self.request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['email'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home:home')
+            elif user is None:
+                messages.error(request, 'ایمیل یا رمز عبور اشتباه است', 'danger')
+                return redirect('home:home')
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from accounts.forms import UserLoginForm, UserRegisterForm
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')
-
+        register_form = UserRegisterForm
+        login_form = UserLoginForm
+        return render(request, 'home/index.html', {
+            'register_form': register_form, 'login_form': login_form
+        })
